bdc1231f2e710ed97e02a7126c484115977852f9
pyaxy_mac <pyaxy0410@gmail.com>
2025-03-21 21:26:23 +0800

    feat(user_auth): add polls demo.
    Changes the timezone to Shanghai.
    Changes language to zh-hans

--- CodeCollab/settings.py
+++ CodeCollab/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -112,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "zh-hans"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Shanghai"
 
 USE_I18N = True
 
